ccb58565b9a22cc68fa8646492f29d2ac4965ec2
CHUAN HE <fantasylink@gmail.com>
2024-01-30 18:27:53 -0800

    begin

--- Augular-Portfolio/src/app/app.component.ts
+++ Augular-Portfolio/src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+export class Category {
+  id!: number;
+  name!: string;
+  slug!: string;
+}
+
+export class Tag {
+  id!: number;
+  name!: string;
+  slug!: string;
+  pivot?: any;
+}
+
+export class Project {
+  'id': number;
+  'title': string;
+  'slug': string;
+  'excerpt': string;
+  'body': string;
+  'url': string | null;
+  'published_date': string | null;
+  'image': string | null;
+  'thumb': string | null;
+  'category_id': number | null;
+  'created_at': string;
+  'updated_at': string;
+  'category': Category | null;
+  'tags': Tag[] | undefined;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Augular-Portfolio';
+  title = "Chuan's showcase";
 }
